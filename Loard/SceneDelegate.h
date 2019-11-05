739627767d5540f510450030757acb0718405033
superlili <isnewpengtao@gmail.com>
2019-11-05 14:54:44 +0800

    1
    1

---
+++ Loard/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Loard
+//
+//  Created by 李莉 on 2019/11/5.
+//  Copyright © 2019 ebtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Loard/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Loard
+//
+//  Created by 李莉 on 2019/11/5.
+//  Copyright © 2019 ebtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
